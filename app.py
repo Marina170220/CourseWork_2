d5dee58b5552a5d1ca7d26a72f176d0ed0bcd930
Marina <petrukmn@yandex.ru>
2022-01-14 22:59:57 +0300

    Fix form in step 3, add steps with *

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 from functions import *
 app = Flask(__name__)
 
 
-@app.route('/',)
+@app.route('/')
 def page_index():
     posts = get_posts_with_comments_count()
-    return render_template('index.html', posts=posts)
+    bookmarks = get_data('data/bookmarks.json')
+    bookmarks_count = len(bookmarks)
+    return render_template('index.html', posts=posts, bookmarks_count=bookmarks_count)
 
 
 @app.route('/posts/<int:pk>',)
@@ -18,9 +20,7 @@ def page_post(pk):
 
 @app.route('/search/', methods=["GET", "POST"])
 def page_search():
-    if request.method == "POST":
-        search = request.form.get('search')
-    elif request.method == "GET":
+    if request.method == "GET":
         search = request.args.get('s')
     if search:
         found_posts = get_posts_by_word(search)
@@ -33,10 +33,48 @@ def page_search():
     return 'Вы не ввели запрос'
 
 
-@app.route('/users/<username>',)
+@app.route('/users/<username>')
 def page_user(username):
     posts = get_posts_by_user(username)
     return render_template('user-feed.html', posts=posts, username=username)
+
+
+@app.route('/tag/<tagname>')
+def page_tag(tagname):
+    posts = get_posts_by_tag(tagname)
+    return render_template('tag.html', posts=posts, tagname=tagname)
+
+
+@app.route('/bookmarks/add/<int:postid>')
+def page_bookmarks_add(postid):
+    add_bookmark(postid)
+    return redirect("/", code=302)
+
+
+@app.route('/bookmarks/remove/<int:postid>')
+def page_bookmarks_remove(postid):
+    remove_bookmark(postid)
+    return redirect("/", code=302)
+    # posts = get_posts_with_hashtags()
+    # return render_template('index.html', posts=posts)
+
+
+@app.route('/bookmarks/')
+def page_bookmarks():
+    posts = get_data('data/bookmarks.json')
+    count = len(posts)
+    if posts:
+        return render_template('bookmarks.html', posts=posts, count=count)
+    return "В закладках пока нет ни одного поста"
+
+
+# @app.route("/tag")
+# def page_tag():
+#     tag = request.args.get('tag')
+#     if tag:
+#         posts = get_posts_by_tag(read_json(POST_PATH), tag)
+#         return render_template("post_by_tag.html", posts=posts, tag=tag)
+#     return abort(400, "Вы не выбрали ни одного тега :(")
 
 
 if __name__ == "__main__":
--- functions.py
+++ functions.py
@@ -3,18 +3,20 @@ import pprint
 
 
 def get_data(file):
+    """
+    Получает данные из json файла.
+    :param file: файл с данными.
+    :return:  данные в json формате.
+    """
     with open(file, 'r', encoding='UTF-8') as f:
         return json.load(f)
 
 
-def get_post_by_pk(pk):
-    posts = get_posts_with_comments_count()
-    for post in posts:
-        if post.get('pk') == pk:
-            return post
-
-
 def get_posts_with_comments_count():
+    """
+    На основе списков постов и комментариев добавляет количество комментариев для каждого поста.
+    :return: список постов с числом комментариев.
+    """
     posts = get_data('data/posts.json')
     comments = get_data('data/comments.json')
     comments_count = {}
@@ -38,13 +40,40 @@ def get_posts_with_comments_count():
     return posts
 
 
+# def
+
+
+def get_post_by_pk(pk):
+    """
+    Находит пост по его id.
+    :param pk: id поста.
+    :return: соответсвующий пост.
+    """
+    posts = get_posts_with_comments_count()
+    for post in posts:
+        if post.get('pk') == pk:
+            post['content'] = replace_hashtags_with_links(post['content'])
+            return post
+
+
 def get_post_comments(pk):
+    """
+    Формирует список комментариев к посту.
+    :param pk: id поста.
+    :return: список комментариев к этому посту.
+    """
     comments = get_data('data/comments.json')
     post_comments = [comment for comment in comments if comment.get('post_id') == pk]
     return post_comments
 
 
 def set_ending(count):
+    """
+    Возвращает окончание слова в нужном склонении в зависимости от количества комментариев.
+    Предназначен для корректного отображения числа комментариев к посту на странице.
+    :param count: количество комментариев.
+    :return: окончание слова "комментарий".
+    """
     if count == 1 or count % 10 == 1 and count % 100 != 11:
         return 'й'
     elif 2 <= count <= 4 or 2 <= count % 10 <= 4 and (count % 100 < 10 or count % 100 > 20):
@@ -54,15 +83,131 @@ def set_ending(count):
 
 
 def get_posts_by_word(word):
+    """
+    Находит посты по вхождению ключевого слова.
+    :param word: ключевое слово для поиска.
+    :return: список постов, содержащих ключевое слово.
+    """
     posts = get_posts_with_comments_count()
     posts_by_word = [post for post in posts if word.lower() in post.get('content').lower()]
     return posts_by_word
 
 
+# def get_posts_by_tag(tag):
+#     """
+#     Находит посты по вхождению тега.
+#     :param tag: тег для поиска.
+#     :return: список постов, содержащих тег.
+#     """
+#     posts = get_posts_with_hashtags()
+#     posts_by_tag = [post for post in posts if tag.lower() in post.get('content').lower()]
+#     return posts_by_tag
+
+
+def get_posts_by_tag(tag):
+    """
+    Формирует список постов по выбранному тегу.
+    :param tag: выбранный тег.
+    :return: список найденных постов.
+    """
+    posts = get_posts_with_comments_count()
+    return [post for post in posts if f'#{tag}' in post['content']]
+
+
 def get_posts_by_user(user):
+    """
+    Находит посты, опубликованные определённым пользователем.
+    :param user: имя пользователя.
+    :return: список постов искомого пользователя.
+    """
     posts = get_posts_with_comments_count()
     posts_by_user = [post for post in posts if user.lower() in post.get('poster_name').lower()]
     return posts_by_user
 
-#
-# pprint.pprint(get_posts_by_user('leo'))
+
+def replace_hashtags_with_links(content):
+    """
+    Заменяет хештеги в посте ссылками.
+    :param content: текст поста.
+    :return: текст поста с кликабельными хештегами.
+    """
+    words_in_post = content.split(' ')
+    for index, word in enumerate(words_in_post):
+        if word.startswith('#'):
+            tag = word[1:]
+            words_in_post[index] = f"<a href='/tag/{tag}'>{word}</a>"
+    return " ".join(words_in_post)
+
+
+# def get_posts_with_hashtags():
+#     """
+#     Заменяет текст поста на текст со ссылками.
+#     :return: список постов с кликабельными хештегами.
+#     """
+#     posts = get_posts_with_comments_count()
+#     for post in posts:
+#         post['content'] = replace_hashtags_with_links(post.get('content'))
+#     return posts
+
+
+def add_bookmark(pk):
+    """
+    Добавляет пост в закладки.
+    :param pk: id добавляемого поста.
+    """
+
+    bookmarks = get_data('data/bookmarks.json')
+
+    posts = get_posts_with_comments_count()
+    for post in posts:
+        if pk == post.get('pk'):
+            if not is_post_in_list_check(pk, bookmarks):
+                bookmarks.append(post)
+
+    with open('data/bookmarks.json', 'w', encoding='UTF-8') as f:
+        json.dump(bookmarks, f, ensure_ascii=False, indent=4)
+
+    # return bookmarks
+
+
+    # is_post_in_bookmarks = False
+    #
+    # for post in bookmarks:
+    #     if pk == post.get('pk'):
+    #         is_post_in_bookmarks = True
+
+
+def remove_bookmark(pk):
+    """
+    Удаляет пост из закладок.
+    :param pk: id удаляемого поста.
+    """
+    bookmarks = get_data('data/bookmarks.json')
+
+    if bookmarks:
+        for post in bookmarks:
+            if pk == post.get('pk'):
+                bookmarks.remove(post)
+
+    with open('data/bookmarks.json', 'w', encoding='UTF-8') as f:
+        json.dump(bookmarks, f, ensure_ascii=False, indent=4)
+
+
+    # json.dump(bookmarks, fp, ensure_ascii=False, indent=4)
+    # fp.close()
+
+
+def is_post_in_list_check(id_, list_):
+    """
+    Проверяет, находится ли пост в списке
+    :param id_: id поста
+    :param list_: список постов
+    :return: True, если искомый пост есть в списке, иначе False
+    """
+    for post in list_:
+        if id_ == post.get('pk'):
+            return True
+    return False
+
+
+pprint.pprint(add_bookmark(1))
